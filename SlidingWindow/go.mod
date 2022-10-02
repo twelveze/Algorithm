8aa57b76e328a33bb43dd42ebf212d6deb19dac4
twelveze <a1997529@vip.qq.com>
2022-10-02 14:49:56 +0800

    fix mod error

---
+++ SlidingWindow/go.mod
@@ -0,0 +1,6 @@
+module Algorithm/SlidingWindow
+
+go 1.18
+
+require Algorithm/Model v1.0.0
+replace Algorithm/Model => ../Model
--- SlidingWindow/lengthOfLongestNorepeatSubstring.go
+++ SlidingWindow/lengthOfLongestNorepeatSubstring.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"Algorithm/Algorithmtest/Model"
+	"Algorithm/Model"
 	"fmt"
 )
 
-func lengthOfLongestNoRepeatSubstring(str string) int{
+func lengthOfLongestNoRepeatSubstring(str string) int {
 	index := [128]int{}
-	for i, _ := range index{
+	for i, _ := range index {
 		index[i] = 0
 	}
-	if len(str) == 0{
+	if len(str) == 0 {
 		return 0
 	}
 	ans := 0
-	for start, end := 0, 0; end < len(str); end++{
-		if index[str[end]] != 0{
-			start = Model.Max(start,index[str[end]])
+	for start, end := 0, 0; end < len(str); end++ {
+		if index[str[end]] != 0 {
+			start = Model.Max(start, index[str[end]])
 		}
-		if (end - start + 1) > ans{
+		if (end - start + 1) > ans {
 			ans = end - start + 1
 		}
 		index[str[end]] = end + 1
